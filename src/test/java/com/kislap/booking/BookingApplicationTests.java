aae9f27db6774033ddc0bf11269b52604c07184b
Cabubas, Christian Joseph C. <cjplaysgames83@gmail.com>
2025-09-13 21:53:06 +0800

    Spring Boot initial project setup

---
+++ src/test/java/com/kislap/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kislap.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

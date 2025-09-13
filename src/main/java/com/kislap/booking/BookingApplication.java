6fa7af934f2b30a9ab103ebbf0ea6131bae23477
Cabubas, Christian Joseph C. <cjplaysgames83@gmail.com>
2025-09-13 22:06:39 +0800

    disable database auto-configuration for now

--- src/main/java/com/kislap/booking/BookingApplication.java
+++ src/main/java/com/kislap/booking/BookingApplication.java
@@ -3,6 +3,9 @@ package com.kislap.booking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// disable database auto-configuration for now
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
+
 public class BookingApplication {
 
 	public static void main(String[] args) {
